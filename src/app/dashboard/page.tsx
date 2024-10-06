f227d49519ee20d1249084b16c686e4412f98a3c
Joakim Vu <joakimvu@icloud.com>
2024-10-06 19:05:34 +0200

    Update page.tsx
    styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,37 +8,49 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { CirclePlus } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center h-screen text-center gap-6 max-w-5xl mx-auto">
-      <h1 className="text-5xl font-bold">Dashboard</h1>
+    <main className="flex flex-col h-screen text-center gap-6 max-w-5xl mx-auto my-12">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Invoices</h1>
+        <p>
+          <Button variant="ghost" className="inline-flex gap-2" asChild>
+            <Link href="/invoices/new">
+              <CirclePlus className="h-4 w-4" /> Create Invoice
+            </Link>
+          </Button>
+        </p>
+      </div>
       <Table>
         <TableCaption>A list of your recent invoices.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className="text-left w-[100px]">Date</TableHead>
-            <TableHead className="text-left">Customer</TableHead>
-            <TableHead className="text-left">Email</TableHead>
-            <TableHead className="text-center">Status</TableHead>
-            <TableHead className="text-right">Amount</TableHead>
+            <TableHead className="text-left w-[100px] p-4">Date</TableHead>
+            <TableHead className="text-left p-4">Customer</TableHead>
+            <TableHead className="text-left p-4">Email</TableHead>
+            <TableHead className="text-center p-4">Status</TableHead>
+            <TableHead className="text-right p-4">Amount</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           <TableRow>
-            <TableCell className="text-left font-medium">
+            <TableCell className="text-left font-medium p-4">
               <span className="font-semibold">10/10/2024</span>
             </TableCell>
-            <TableCell className="text-left">
+            <TableCell className="text-left p-4">
               <span className="font-semibold">John Doe</span>
             </TableCell>
-            <TableCell className="text-left">
+            <TableCell className="text-left p-4">
               <span>johndoe@example.com</span>
             </TableCell>
-            <TableCell className="text-center">
+            <TableCell className="text-center p-4">
               <Badge className="rounded-full">Open </Badge>
             </TableCell>
-            <TableCell className="text-right">
+            <TableCell className="text-right p-4">
               <span>$250.00</span>
             </TableCell>
           </TableRow>
