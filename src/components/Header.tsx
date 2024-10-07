8969563062c4df8f99673c4672ddfa85cc1f459f
Joakim Vu <joakimvu@icloud.com>
2024-10-07 16:19:39 +0200

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Container from "./Container";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="mt-8 mb-12">
+      <Container>
+        <div className="flex justify-between items-center gap-4">
+          <p className="font-bold">
+            <Link href="/dashboard"> Invoice Shark</Link>
+          </p>
+          <div>
+            <SignedOut>
+              <SignInButton />
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
