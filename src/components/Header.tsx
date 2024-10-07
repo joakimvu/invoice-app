3a5fb72312aef2cdc3da7ff85890624e0957bf49
Joakim Vu <joakimvu@icloud.com>
2024-10-07 21:24:19 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,4 @@
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Container from "./Container";
 import Link from "next/link";
 
