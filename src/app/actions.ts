c292a7077738f99b1f529ba5c52a80bd22bb58e9
Joakim Vu <joakimvu@icloud.com>
2024-10-08 15:47:06 +0200

    Update actions.ts

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,19 +1,26 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 import { Invoices } from "@/db/schema";
 import { db } from "@/db";
 
 export async function createAction(formData: FormData) {
+  const { userId } = auth();
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   const description = formData.get("description") as string;
+
+  if (!userId) {
+    return;
+  }
 
   const results = await db
     .insert(Invoices)
     .values({
       value,
       description,
+      userId,
       status: "open",
     })
     .returning({
