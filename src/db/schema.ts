ce4760cc6e0c485b37bfdd823c3afa0c31e929df
Joakim Vu <joakimvu@icloud.com>
2024-10-08 15:47:13 +0200

    Update schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -18,6 +18,7 @@ export const Invoices = pgTable("invoices", {
   id: serial("id").primaryKey().notNull(),
   createTimeStamp: timestamp("createTimeStamp").defaultNow().notNull(),
   value: integer("value").notNull(),
-  description: text("description"),
+  description: text("description").notNull(),
+  userId: text("userId").notNull(),
   status: statusEnum("status").notNull(),
 });
