e24e729f75a1e9b72649e3114ee9151ca020cba5
Joakim Vu <joakimvu@icloud.com>
2024-10-08 15:47:10 +0200

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -15,10 +15,18 @@ import { db } from "@/db";
 import { Invoices } from "@/db/schema";
 import { cn } from "@/lib/utils";
 import Container from "@/components/Container";
+import { auth } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 
 export default async function Home() {
-  const results = await db.select().from(Invoices);
-  console.log("results", results);
+  const { userId } = auth();
+
+  if (!userId) return;
+
+  const results = await db
+    .select()
+    .from(Invoices)
+    .where(eq(Invoices.userId, userId));
 
   return (
     <main className="h-full">
