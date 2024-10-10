a2faa672cab886260f6974d31b502048440aa5f6
Joakim Vu <joakimvu@icloud.com>
2024-10-10 15:45:04 +0200

    Update page.tsx

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/db";
-import { Invoices } from "@/db/schema";
+import { Customers, Invoices } from "@/db/schema";
 import { auth } from "@clerk/nextjs/server";
-import { and, eq } from "drizzle-orm";
+import { and, eq, isNull } from "drizzle-orm";
 import { notFound } from "next/navigation";
 
 import Invoice from "./invoice";
@@ -11,22 +11,48 @@ export default async function InvoicePage({
 }: {
   params: { invoiceId: string };
 }) {
-  const { userId } = auth();
+  const { userId, orgId } = auth();
   const invoiceId = parseInt(params.invoiceId);
 
   if (isNaN(invoiceId)) {
     throw new Error("Invalid Invoice ID");
   }
 
-  const [result] = await db
-    .select()
-    .from(Invoices)
-    .where(and(eq(Invoices.id, invoiceId), eq(Invoices.userId, userId)))
-    .limit(1);
+  let result;
+  if (orgId) {
+    [result] = await db
+      .select()
+      .from(Invoices)
+      .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
+      .where(
+        and(eq(Invoices.id, invoiceId), eq(Invoices.organizationId, orgId))
+      )
+      .limit(1);
+  } else {
+    [result] = await db
+      .select()
+      .from(Invoices)
+      .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
+      .where(
+        and(
+          eq(Invoices.id, invoiceId),
+          eq(Invoices.userId, userId),
+          isNull(Invoices.organizationId)
+        )
+      )
+      .limit(1);
+  }
 
   if (!result) {
     notFound();
   }
 
-  return <Invoice invoice={result} />;
+  const invoice = {
+    ...result.invoices,
+    customer: result.customers,
+  };
+
+  console.log("result", result);
+
+  return <Invoice invoice={invoice} />;
 }
