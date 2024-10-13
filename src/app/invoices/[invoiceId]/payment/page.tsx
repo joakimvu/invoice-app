60aa9e0430ff2811e1abf1f6f9a296fed47fa7e0
Joakim Vu <joakimvu@icloud.com>
2024-10-13 17:42:21 +0200

    Update page.tsx

--- src/app/invoices/[invoiceId]/payment/page.tsx
+++ src/app/invoices/[invoiceId]/payment/page.tsx
@@ -11,9 +11,15 @@ import { Button } from "@/components/ui/button";
 import { Check, CreditCard } from "lucide-react";
 import { createPayment, updateStatusAction } from "@/app/actions";
 
+import Stripe from "stripe";
+const stripe = new Stripe(String(process.env.STRIPE_API_SECRET));
+
 interface InvoicePageProps {
   params: { invoiceId: string };
-  searchParams: { status: string };
+  searchParams: {
+    status: string;
+    session_id: string;
+  };
 }
 
 export default async function InvoicePage({
@@ -22,8 +28,12 @@ export default async function InvoicePage({
 }: InvoicePageProps) {
   const invoiceId = parseInt(params.invoiceId);
 
-  const isSuccess = searchParams.status === "success";
+  const sessionId = searchParams.session_id;
+  const isSuccess = sessionId && searchParams.status === "success";
   const isCanceled = searchParams.status === "canceled";
+  let isError = isSuccess && !sessionId;
+
+  console.log(searchParams);
 
   console.log("isSuccess", isSuccess);
   console.log("isCanceled", isCanceled);
@@ -33,10 +43,18 @@ export default async function InvoicePage({
   }
 
   if (isSuccess) {
-    const formData = new FormData();
-    formData.append("id", String(invoiceId));
-    formData.append("status", "paid");
-    await updateStatusAction(formData);
+    const { payment_status } = await stripe.checkout.sessions.retrieve(
+      sessionId
+    );
+
+    if (payment_status !== "paid") {
+      isError = true;
+    } else {
+      const formData = new FormData();
+      formData.append("id", String(invoiceId));
+      formData.append("status", "paid");
+      await updateStatusAction(formData);
+    }
   }
 
   const [result] = await db
@@ -67,6 +85,18 @@ export default async function InvoicePage({
   return (
     <main className="wg-full h-full">
       <Container>
+        {isError && (
+          <p className="bg-red-100 text-sm text-red-800 text-center px-3 py-2 rounded-lg mb-6">
+            Something went wrong, please try again
+          </p>
+        )}
+
+        {isCanceled && (
+          <p className="bg-yellow-100 text-sm text-yellow-800 text-center px-3 py-2 rounded-lg mb-6">
+            Payment was canceled, please try again.
+          </p>
+        )}
+
         <div className="grid grid-cols-2">
           <div>
             <div className="flex justify-between mb-8">
